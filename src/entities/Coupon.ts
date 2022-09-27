58616cecb245cf873aa62cb8a68abdec35589e9c
Grasiela Gomes <produtoradama@gmail.com>
2022-09-27 18:42:53 -0300

    feat: implement validateDate

--- src/entities/Coupon.ts
+++ src/entities/Coupon.ts
@@ -1,19 +1,28 @@
 export interface CouponI {
   couponId: string;
+  expiration?: Date | string | number;
   percentage: number;
 }
 
 export class Coupon {
   readonly couponId: string;
+  readonly expiration?: Date | string | number;
   readonly percentage: number;
 
-  constructor({ couponId, percentage }: CouponI) {
+  constructor({ couponId, expiration, percentage }: CouponI) {
     this.couponId = this.validateId(couponId);
+    this.expiration = expiration && this.validateDate(expiration);
     this.percentage = this.validatePercentage(percentage);
   }
 
-  applyCoupon (total: number): number {
-    return total * this.percentage / 100;
+  applyCoupon(total: number): number {
+    return (total * this.percentage) / 100;
+  }
+
+  private validateDate(value: Date | string | number) {
+    const date = new Date(value).toString();
+    if (date !== "Invalid Date") return new Date(value);
+    throw new Error("invalid date");
   }
 
   private validateId(id: string): string {
