b65e6c1031e445368509d6ebfbccdf754ce54dc3
Grasiela Gomes <produtoradama@gmail.com>
2022-09-28 18:30:27 -0300

    test: add new test

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,33 +1,63 @@
 import { Coupon } from "../src/entities/Coupon";
-import { Coupons } from "../src/entities/Coupons";
 import { Order } from "../src/entities/Order";
 
 describe("Creating orders", () => {
   const product1 = {
     _id: "1",
-    name: "product1",
     description: "Description 1",
-    price: 1000
+    height: 100,
+    length: 10,
+    name: "guitar",
+    price: 1000,
+    weight: 3,
+    width: 30
   };
 
   const product2 = {
     _id: "2",
-    name: "product2",
     description: "Description 2",
-    price: 2000
+    height: 20,
+    length: 10,
+    name: "camera",
+    price: 2000,
+    weight: 1,
+    width: 15
   };
 
   const product3 = {
     _id: "3",
-    name: "product3",
     description: "Description 3",
-    price: 500
+    height: 200,
+    length: 50,
+    name: "refrigerator",
+    price: 500,
+    weight: 40,
+    width: 100
   };
 
   it("should not create an order if CPf is invalid", () => {
     const invalidCPF = "111111111-11";
     const newOrder = new Order(invalidCPF);
     expect(() => newOrder.document.isValid()).toThrow(new Error("invalid CPF"));
+  });
+
+  it("should not create an order if the quantity is less than 1", () => {
+    const cpf = "259.556.978-37";
+    const newOrder = new Order(cpf);
+    expect(() => newOrder.addItem(product1, 0)).toThrow(
+      new Error("The quantity needs to be equal or greater than 1")
+    );
+  });
+
+  it("should not create the same product twice but increase the quantity", () => {
+    const cpf = "259.556.978-37";
+    const newOrder = new Order(cpf);
+    newOrder.addItem(product1, 1);
+    newOrder.addItem(product1, 1);
+    newOrder.addItem(product1, 1);
+    expect(newOrder.orderItems).toEqual([
+      { itemId: "1", price: 1000, quantity: 3 }
+    ]);
   });
 
   it("should create an order with 3 items including description, price and quantity", () => {
