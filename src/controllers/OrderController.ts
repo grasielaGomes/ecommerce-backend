a3cebfe66028801994f8fcf55988403c1469d555
Grasiela Gomes <produtoradama@gmail.com>
2022-10-06 11:26:52 -0300

    feat: add getOrdersByCPF call

--- src/controllers/OrderController.ts
+++ src/controllers/OrderController.ts
@@ -1,20 +1,31 @@
 import HttpServer from "../http/HttpServer";
 import Preview from "../useCases/Preview";
 import Checkout from "../useCases/Checkout";
+import GetOrdersByCPF from "../useCases/GetOrdersByCPF";
 
 export default class OrderController {
   constructor(
     readonly httpServer: HttpServer,
     readonly preview: Preview,
-    readonly checkout: Checkout
+    readonly checkout: Checkout,
+    readonly getOrderByCpf: GetOrdersByCPF
   ) {
-    httpServer.on("post", "/preview", async (params: any, body: any) => {
+    httpServer.on("post", "/preview", async function (params: any, body: any) {
       const total = await preview.execute(body);
       return { total };
     });
-    httpServer.on("post", "/checkout", async (params: any, body: any) => {
-      const total = await checkout.execute(body);
-      return { total };
+
+    httpServer.on("post", "/checkout", async function (params: any, body: any) {
+      await checkout.execute(body);
     });
+
+    httpServer.on(
+      "get",
+      "/orders/:cpf",
+      async function (params: any, body: any) {
+        const orders = await getOrderByCpf.execute(params.cpf);
+        return orders;
+      }
+    );
   }
 }
