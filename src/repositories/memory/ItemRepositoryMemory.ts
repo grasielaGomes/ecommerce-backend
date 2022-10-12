b55616a51e82c34d74586eb714f40b6a53b3fe9a
Grasiela Gomes <produtoradama@gmail.com>
2022-10-12 13:15:26 -0300

    feat: add getTotalVolume and getTotalDensity methods

--- src/repositories/memory/ItemRepositoryMemory.ts
+++ src/repositories/memory/ItemRepositoryMemory.ts
@@ -1,18 +1,35 @@
-import { ItemI } from "../../entities";
+import { ItemI, Item } from "../../entities";
 import ItemRepository from "../interfaces/ItemRepository";
 
 export default class ItemRepositoryMemory implements ItemRepository {
-  items: ItemI[];
+  items: Item[];
   constructor() {
     this.items = [];
   }
-  async getItem(idItem: string): Promise<ItemI> {
+  async getItem(idItem: string): Promise<Item> {
     const item = this.items.find((item) => item._id === idItem);
     if (!item) throw new Error("Item not found");
     return item;
   }
 
   async save(item: ItemI): Promise<void> {
-    this.items.push(item);
+    const newItem = new Item(item);
+    this.items.push(newItem);
+  }
+
+  getTotalVolume(): number {
+    let total = 0;
+    for (const item of this.items) {
+      total += item.getVolume();
+    }
+    return total;
+  }
+
+  getTotalDensity(): number {
+    let total = 0;
+    for (const item of this.items) {
+      total += item.getDensity();
+    }
+    return total;
   }
 }
