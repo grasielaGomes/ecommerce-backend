b41481ba569bcdb86278f84f48deb01dc04ed56f
Grasiela Gomes <produtoradama@gmail.com>
2022-10-12 13:10:03 -0300

    refactor: add coupon into input type

--- src/useCases/Preview.ts
+++ src/useCases/Preview.ts
@@ -1,8 +1,12 @@
 import { Order } from "../entities";
 import ItemRepository from "../repositories/interfaces/ItemRepository";
+import CouponRepository from "../repositories/interfaces/CouponRepository";
 
 export default class Preview {
-  constructor(readonly itemRepository: ItemRepository) {}
+  constructor(
+    readonly itemRepository: ItemRepository,
+    readonly couponRepository: CouponRepository
+  ) {}
 
   async execute(input: Input): Promise<number> {
     const order = new Order(input.cpf);
@@ -10,11 +14,16 @@ export default class Preview {
       const item = await this.itemRepository.getItem(orderItem.idItem);
       order.addItem(item, orderItem.quantity);
     }
+    if(input.coupon) {
+      const coupon = await this.couponRepository.getCoupon(input.coupon);
+      return order.applyDiscount(coupon);
+    }
     return order.getTotal();
   }
 }
 
 type Input = {
+  coupon?: string;
   cpf: string;
   orderItems: { idItem: string; quantity: number }[];
 };
